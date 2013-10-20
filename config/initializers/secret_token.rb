6809d14a9071c041a2241e51156ce3f1dd11c553
yxt423 <yxt423@gmail.com>
2013-10-20 13:18:13 -0400

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trydevise::Application.config.secret_token = '488b37af1bdff5394f0d16b45975447d0c5e7737ea61c883dfe14625879f128dc9efa6493cc9b667a391d49b6153f165799a3fe6e9ff31f6935748448c04a352'
